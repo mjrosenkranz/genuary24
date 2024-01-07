907edde877815852c6f98f44be8ff2a75888fde2
Morgan Rosenkranz <rosenkranz1999@gmail.com>
2024-01-07 16:46:37 -0500

    bounce the ball

--- sketches/3_droste/src/main.rs
+++ sketches/3_droste/src/main.rs
@@ -1,5 +1,5 @@
 use bevy::{
-    prelude::*,
+    prelude::*, sprite::MaterialMesh2dBundle,
 };
 
 pub mod sketch_plugin;
@@ -7,13 +7,62 @@ use crate::sketch_plugin::*;
 
 fn main() {
     App::new()
-        .add_plugins(SketchSetupPlugin)
-        .add_systems(Update, update)
+        .add_plugins(SketchSetupPlugin{
+            sketch_type: SketchType::Sketch2d,
+        })
+        .add_systems(Startup, setup)
+        .add_systems(Update, bounce_circle)
         .run();
 }
 
-
-fn update(
-    time: Res<Time>,
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    // outer scene
+    // spawn a 2d camera
+    commands.spawn(Camera2dBundle::default());
+
+    // make a material for the square
+    // TODO: make material a texture?
+    let material = materials.add(ColorMaterial {
+        color: Color::rgb(0.0, 1.0, 0.0),
+        // texture: ()
+        ..default()
+    });
+    // spawn a square
+    let mesh = meshes.add(shape::Circle::new(50.0).into());
+
+    commands.spawn((MaterialMesh2dBundle {
+        mesh: mesh.into(),
+        material,
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+        ..default()
+    },
+    Circle {
+        vel: Vec3::new(1.0, -2.0, 0.0).normalize() * 250.0,
+    }));
+}
+
+#[derive(Component)]
+struct Circle {
+    vel: Vec3,
+}
+
+fn bounce_circle(
+    time: Res<Time>,
+    mut circle_q: Query<(&mut Transform, &mut Circle)>,
+) {
+    let (mut transform, mut circle) = circle_q.get_single_mut().unwrap();
+    let next_pos = transform.translation + circle.vel * time.delta_seconds();
+
+    if next_pos.x >= 200.0 || next_pos.x <= -200.0 {
+        circle.vel.x *= -1.0;
+    }
+    if next_pos.y >= 200.0 || next_pos.y <= -200.0 {
+        circle.vel.y *= -1.0;
+    }
+
+    transform.translation += circle.vel * time.delta_seconds();
 }
--- sketches/3_droste/src/sketch_plugin.rs
+++ sketches/3_droste/src/sketch_plugin.rs
@@ -2,27 +2,47 @@ use bevy::{
     prelude::*, DefaultPlugins, render::camera::ScalingMode,
 };
 
-pub struct SketchSetupPlugin;
+pub enum SketchType {
+    Sketch2d,
+    Sketch3d,
+}
+
+pub struct SketchSetupPlugin{
+    pub sketch_type: SketchType,
+}
 
 impl Plugin for SketchSetupPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_plugins(DefaultPlugins)
             .add_systems(Startup, setup_window);
+
+        match self.sketch_type {
+            SketchType::Sketch3d => {app.add_systems(Startup, setup_3d);},
+            _ => {},
+        }
     }
 }
 
 fn setup_window(
-    mut commands: Commands,
     mut clear_color: ResMut<ClearColor>,
     mut windows: Query<&mut Window>,
 ) {
+    // setup the camera
+    let mut window = windows.single_mut();
+    window.resolution.set(500.0, 500.0);
+    clear_color.0 = Color::BLACK;
+}
 
+
+fn setup_3d(
+    mut commands: Commands,
+) {
     let origin = Vec3::new(0.0, 25.0, 0.0);
     let dist: f32 = 50.0;
     let height: f32 = dist * 0.8534 + origin.y;
 
-    // setup the camera
+
     commands.spawn((Camera3dBundle {
         camera: Camera {
             hdr: true,
@@ -38,8 +58,4 @@ fn setup_window(
     },
     ));
 
-    let mut window = windows.single_mut();
-    window.resolution.set(500.0, 500.0);
-    clear_color.0 = Color::BLACK;
 }
-
